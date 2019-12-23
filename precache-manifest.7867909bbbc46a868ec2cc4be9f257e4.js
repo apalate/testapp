f3066c114452f882f8cab3bc006eac45973911cd
witusj <wfth69@gmail.com>
2019-12-23 16:09:10 +0100

    Updates

---
+++ precache-manifest.7867909bbbc46a868ec2cc4be9f257e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80cd3351dd93a95c03651eedcaf0031",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "7f9d766cf73b024aa089",
+    "url": "/testapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf1f34b96479781184a8",
+    "url": "/testapp/static/js/2.994851ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testapp/static/js/2.994851ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f9d766cf73b024aa089",
+    "url": "/testapp/static/js/main.34ce8a18.chunk.js"
+  },
+  {
+    "revision": "643f6c33c67157951d51",
+    "url": "/testapp/static/js/runtime-main.59ecec2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
